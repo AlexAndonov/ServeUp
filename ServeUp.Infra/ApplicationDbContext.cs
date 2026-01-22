98614aa87d328293dbf80fdf48d77bc42456c42d
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-01-22 21:43:05 +0200

    Add ApplicationDbContext

---
+++ ServeUp.Infra/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServeUp.Infra
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) 
+			: base(options)
+		{
+			
+		}
+	}
+}
